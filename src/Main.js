f0c2047d473543428be1688107c2b2df5cdff192
Aaron Rutherford <rutherfordaaron1@gmail.com>
2022-08-07 15:27:26 -0700

    Initialization of the project

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import "./css/Main.css";
+
+const Main = () => {
+  return (
+    <main>
+      <h1>Header 1</h1>
+      <h2>Header 2</h2>
+      <p>Paragraph</p>
+    </main>
+  )
+}
+
+export default Main;
