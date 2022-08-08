888a9c33a6aed8cb8357150babf274614809d134
Aaron Rutherford <rutherfordaaron1@gmail.com>
2022-08-07 23:43:07 -0700

    Completed the contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,37 @@
 import "../css/Contact.css";
+import { motion } from "framer-motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const Contact = () => {
+  const contactHover = {
+    on: { scale: 1.1, rotate: 15 },
+    off: { scale: 1, rotate: 0 }
+  }
   return (
     <div class="section flex column">
       <h2>Get in Touch</h2>
+      <div className="contactContainer flex">
+        <motion.a
+          href="mailto:rutherfordaaron1@gmail.com"
+          variants={contactHover}
+          initial="off"
+          whileHover="on"
+          exit="off"
+        >
+          <FontAwesomeIcon icon={faEnvelope} />
+        </motion.a>
+        <motion.a
+          href="tel:+14352320944"
+          variants={contactHover}
+          initial="off"
+          whileHover="on"
+          exit="off"
+        >
+          <FontAwesomeIcon icon={faPhone} />
+        </motion.a>
+      </div>
+      <p className="center">I can be reached best by email or text.</p>
     </div>
   )
 }
