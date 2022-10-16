130e3a2142b3e385c42f04f4001acce23e803e8f
Aaron Rutherford <rutherfordaaron1@gmail.com>
2022-10-15 23:30:16 -0700

    Carousel is mostly working!
    - The carousel is mostly working. Speed and distance traveled across the screen is responsive and the animation is completely linear.
    - A couple bugs where (1) it takes about 2 cycles for images to appear consistently, otherwise sometimes a couple just don't render and (2) when you view another tab or open application that animations break when you go back, but fix after a couple runs

--- components/carousel/carousel.jsx
+++ components/carousel/carousel.jsx
@@ -2,39 +2,56 @@ import Image from "next/future/image";
 import { AnimatePresence, motion } from "framer-motion";
 import styles from "./carousel.module.css";
 
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const Carrousel = ({ images }) => {
-  const imageArr = [...images];
+  const [carousel, setCarousel] = useState([]);
+  const dataFetchedRef = useRef(false);
 
-  const [srcArr, setSrcArr] = useState([...images]);
-  const carrouselDuration = 5;
+  const startCarousel = () => {
+    if (dataFetchedRef.current) return;
+    dataFetchedRef.current = true;
 
-  const addComponent = () => {
-    const tempArr = [...srcArr]
-    tempArr.push(images[0])
-    setSrcArr(tempArr);
+    console.log("starting carousel");
+    let index = 0;
+
+    setInterval(() => {
+      console.log("carousel element created")
+      let arr = [...carousel];
+      arr.push(images[index]);
+      console.log(arr);
+      setCarousel(arr);
+
+      index >= images.length - 1 ? index = 0 : index++;
+    }, 3000)
   }
 
-  const removeComponent = () => {
-    const tempArr = srcArr.slice(1);
-    setSrcArr(tempArr);
+  useEffect(() => startCarousel(), [])
+
+  const getScreenWidth = () => {
+    return window.screen.width;
+  }
+
+  const getAnimationDuration = () => {
+    return window.screen.width / 600 * 5
   }
 
   return (
     <div className={styles.carrousel}>
-      <button onClick={addComponent}>Add Component</button>
-      <button onClick={removeComponent}>Remove Component</button>
-      <AnimatePresence>
-        {srcArr.map((el, i) => {
+      <AnimatePresence className={styles.test}>
+        {carousel.map((el, i) => {
+          const id = Math.random();
           return (
             <motion.div
               className={styles.imageWrapper}
               initial={{ opacity: 0, x: 0 }}
-              animate={{ opacity: 1, x: "calc(50vw - 65px)" }}
-              exit={{ opacity: 0, x: "calc(100vw - 65px)" }}
-              transition={{ ease: "linear" }}
-              key={`carouselImg${i}`}
+              animate={{ opacity: 0, x: 0 }}
+              exit={{ opacity: [0, 1, 0], x: "clamp(110px, calc(100vw - 150px), 725px)" }}
+              transition={{
+                ease: "linear",
+                duration: `${getScreenWidth() >= 800 ? getAnimationDuration() : 8}`
+              }}
+              key={`carouselImg${id}`}
             >
               <Image src={el} alt="test image" width={75} height={75} />
             </motion.div>
