fa92d161e652031cf04b988d1621e5de9db36c25
Aaron Rutherford <rutherfordaaron1@gmail.com>
2022-10-16 21:18:55 -0700

    Created small scroll animations
    - Used the framer-motion property "WhileInView" to create small scroll animations

--- pages/index.jsx
+++ pages/index.jsx
@@ -22,31 +22,50 @@ const Home = () => {
     "/images/logos/mongoose logo.png",
   ]
 
+  const motionVariants = {
+    initial: { x: -20, opacity: 0 },
+    whileInView: { x: 0, opacity: 1 }
+  }
+
+  const motionProps = {
+    variants: motionVariants,
+    initial: "initial",
+    whileInView: "whileInView"
+  }
+
   return (
     <>
       <Head>
         <title>Aaron Rutherford | Aspiring Web Developer</title>
       </Head>
       <Main>
-        <p className="codeDecor"><em>console.log("</em></p>
-        <h1 className={styles.h1}>Hello World!</h1>
-        <p className="codeDecor"><em>");</em></p>
+        <motion.div {...motionProps}>
+          <p className="codeDecor"><em>console.log("</em></p>
+          <h1 className={styles.h1}>Hello World!</h1>
+          <p className="codeDecor"><em>");</em></p>
+        </motion.div>
 
-        <div className={styles.imageWrapper}>
+        <motion.div className={styles.imageWrapper} {...motionProps}>
           <Image
             src="/images/my-images/square-headshot.jpg"
             width={250} height={250}
             className={styles.headshot}
             alt="Aaron Rutherford"
           />
-        </div>
+        </motion.div>
 
-        <p>My name is Aaron Rutherford, and I'm a web developer. I started my web development journey nearly one year ago, and I've come a long way. Take a look!</p>
+        <motion.p {...motionProps}>My name is Aaron Rutherford, and I'm a web developer. I started my web development journey nearly one year ago, and I've come a long way. Take a look!</motion.p>
 
-        <h2 className={styles.h2}>The tools I use:</h2>
-        <p className="codeDecor"><em>&lt;Carousel images=&#123;imgSrcArr&#125;</em></p>
-        <Carousel images={imgSrcArr} />
-        <p className="codeDecor"><em>/&gt;</em></p>
+        <motion.h2 {...motionProps}>My Journey</motion.h2>
+
+        <motion.h2 className={styles.h2} {...motionProps}>The tools I use:</motion.h2>
+        <motion.p {...motionProps}>I have an advanced knowledge of the foundations of the web: HTML, CSS, and JavaScript. I have specialized in the MERN stack and prefer Next.js when building fullstack projects.</motion.p>
+
+        <motion.div {...motionProps}>
+          <p className="codeDecor"><em>&lt;Carousel images=&#123;imgSrcArr&#125;</em></p>
+          <Carousel images={imgSrcArr} />
+          <p className="codeDecor"><em>/&gt;</em></p>
+        </motion.div>
       </Main>
     </>
   )
