c9b7e6a54b08a097fd9c8e5db339a44ac56115af
Aaron Rutherford <rutherfordaaron1@gmail.com>
2022-12-19 18:35:57 -0800

    Content and Semantics for Projects
    - Added information about my current project
    - Used more semantic markdown

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -25,47 +25,61 @@ const Projects = () => {
         <title>Aaron Rutherford | Project</title>
       </Head>
       <Main>
-        <h1>My Projects</h1>
-        <motion.div
-          {...motionProps}
-          className={styles.projectGrid}
-        >
-          <ProjectCard
-            caption="Tip Calculator"
-            description="My first project using React. This app helps you calculate splitting the bill and tip with your group."
-            src={tipCalc}
-            alt="tip calculator project screenshot"
-            link="https://hazipan.github.io/tip-calc/"
-          />
-          <ProjectCard
-            caption="E-Commerce Site"
-            description="This site shows off my ability to achieve pixel perfection from a given design. This is a mock site for an e-commerce site with a functional cart and image gallery."
-            src={eCommerce}
-            alt="static e-commerce project screenshot"
-            link="https://hazipan.github.io/e-commerce-page/"
-          />
-          <ProjectCard
-            caption="Rock, Paper, Scissors, Lizard, Spock"
-            description="An expansion on the classic rock, paper, scissors game. This project really tested my CSS abilities. Your score is stored in your browser to save it between sessions."
-            src={rockPaperScissors}
-            alt="rock, paper, scissors, lizeard, spock project screenshot"
-            link="https://hazipan.github.io/rock-paper-scissors-lizard-spock/"
-          />
-          <ProjectCard
-            caption="Countries API"
-            description="My first exposure to using APIs, this project is connecting to the countries API, allowing you to search and explore all the countries in the world and learn about them."
-            src={countriesAPI}
-            alt="countries API project screenshot"
-            link="https://hazipan.github.io/countries-project-using-api/"
-          />
-          <ProjectCard
-            caption="Todo App"
-            description="A simple todo app with some sorting functionality. This project tested my ability to manipulate arrays and exposed me to creating drag and drop functionality."
-            src={todo}
-            alt="todo project screenshot"
-            link="https://hazipan.github.io/todo-app/"
-          />
-        </motion.div>
+        <article>
+          <motion.h1 {...motionProps}>My Projects</motion.h1>
+          <motion.p {...motionProps}>While I haven't worked in a professional environment yet, or recieved any formal education in Computer Science or Web Development, I've come a long way on my own. I'll let my work speak for itself, so here is a list of projects I have completed and information on what I'm currently working on.</motion.p>
+
+          <section>
+            <motion.h2 {...motionProps}>Current Project and Future Plans</motion.h2>
+            <motion.p {...motionProps}>Right now, I'm currently working on my first fullstack web application. It's going to be a social, recipe sharing application where users can upload and manage recipes in a digital cookbook, but can also share and discover recipes with other people. I also plan to implement meal planning features that will also generate a grocery list based on that meal plan. Then, you will get an estimated price, before you even go to the grocery store. <a href="https://github.com/Hazipan/cookout">See the project on Github.</a></motion.p>
+
+            <motion.p {...motionProps}>This project is still in its infancy. I'm still learning the tools I need to create this project well. I'm currently learning how to implement user sessions and use cookies, as well as enforce server-side password security. Follow me on <a href="https://twitter.com/AaZRuth">Twitter</a> to watch the progress.</motion.p>
+          </section>
+
+          <section>
+            <motion.h2 {...motionProps}>Past Projects</motion.h2>
+            <motion.div
+              {...motionProps}
+              className={styles.projectGrid}
+            >
+              <ProjectCard
+                caption="Tip Calculator"
+                description="My first project using React. This app helps you calculate splitting the bill and tip with your group."
+                src={tipCalc}
+                alt="tip calculator project screenshot"
+                link="https://hazipan.github.io/tip-calc/"
+              />
+              <ProjectCard
+                caption="E-Commerce Site"
+                description="This site shows off my ability to achieve pixel perfection from a given design. This is a mock site for an e-commerce site with a functional cart and image gallery."
+                src={eCommerce}
+                alt="static e-commerce project screenshot"
+                link="https://hazipan.github.io/e-commerce-page/"
+              />
+              <ProjectCard
+                caption="Rock, Paper, Scissors, Lizard, Spock"
+                description="An expansion on the classic rock, paper, scissors game. This project really tested my CSS abilities. Your score is stored in your browser to save it between sessions."
+                src={rockPaperScissors}
+                alt="rock, paper, scissors, lizeard, spock project screenshot"
+                link="https://hazipan.github.io/rock-paper-scissors-lizard-spock/"
+              />
+              <ProjectCard
+                caption="Countries API"
+                description="My first exposure to using APIs, this project is connecting to the countries API, allowing you to search and explore all the countries in the world and learn about them."
+                src={countriesAPI}
+                alt="countries API project screenshot"
+                link="https://hazipan.github.io/countries-project-using-api/"
+              />
+              <ProjectCard
+                caption="Todo App"
+                description="A simple todo app with some sorting functionality. This project tested my ability to manipulate arrays and exposed me to creating drag and drop functionality."
+                src={todo}
+                alt="todo project screenshot"
+                link="https://hazipan.github.io/todo-app/"
+              />
+            </motion.div>
+          </section>
+        </article>
       </Main>
     </>
   )
