5fd0095981fa17621665c7cb7d5a368078b39e97
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-03-06 08:59:13 -0800

    Landing page text and general layout
    Also made it all responsive. I need to fix up the paint bucket and waterfall. Paint bucket needs to increase in size for larger screens and the water fall needs to be replaced with a custom SVG

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,25 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Home = () => {
   return (
     <article>
+      {/* ---------- Landing Section ---------- */}
       <section className="relative h-screen">
         <div>
           {/* Bucket container */}
-          <div className="rotate-[65deg] w-fit absolute top-5 right-5">
+          <div className="rotate-[65deg] w-fit absolute top-5 right-5 md:right-12 lg:right-60">
             <Image className="scale-y-[-1]" src="/bucket.svg" alt="" height={55} width={55} />
           </div>
 
           {/* Paint fall */}
-          <div className="bg-indigo-600 w-[40px] h-[300px] absolute top-[70px] right-[51px] rounded-tl-[100%] rounded-tr-[2px] broder-l-indigo-700 border-r-indigo-700" />
+          <div className="bg-indigo-600 w-[40px] h-[500px] absolute top-[70px] right-[50px] md:right-[78px] lg:right-[271px] md:w-[80px] rounded-tl-[100%] rounded-tr-[2px] broder-l-indigo-700" />
         </div>
 
-        <p className="w-52 text-center relative left-10 top-24">Make a <span className="relative p-2 splash">splash</span> with a site that's designed and developed by the best.</p>
+        {/* Text block 1 */}
+        <div className="max-w-[800px] mx-auto flex pl-5 pt-28 md:pt-20">
+          <p className="w-60 text-center text-xl md:w-96 md:text-3xl">Make a <span className="relative p-2 splash before:w-36 before:h-36 before:bottom-[-55px] before:left-[-40px]">splash</span> with a site that's designed and developed by the best.</p>
+        </div>
 
         {/* Wave border */}
         <div className="wave">
@@ -24,10 +29,21 @@ const Home = () => {
         </div>
 
         {/* Indigo section under the wave */}
-        <div className="h-[50vh] w-screen bg-indigo-600 absolute bottom-0 text-slate-100">
-          <p className="relative left-28 top-10 text-center w-52">Hello world</p>
+        <div className="h-[50vh] w-screen bg-indigo-600 absolute block bottom-0 text-slate-100">
+          <div className="max-w-[800px] mx-auto">
+            {/* Text block 2 */}
+            <div className="flex justify-end pr-5 pt-10">
+              <p className="text-center w-64 text-xl md:w-96 md:text-3xl">Pouring on the skills and creativity to increase your web presence.</p>
+            </div>
+            {/* Action button */}
+            <div className="flex justify-center items-center pt-20">
+              <Link legacyBehavior={false} href="/" className="block bg-indigo-200 px-10 py-5 rounded-full text-gray-800 text-2xl tracking-wider shadow-lg hover:bg-violet-200 hover:text-black md:text-3xl md:py-7 md:px-12">Get a Website</Link>
+            </div>
+          </div>
         </div>
       </section>
+      {/* ---------- Services Section ---------- */}
+      {/* ---------- Work Proof Section ---------- */}
     </article>
   )
 }
