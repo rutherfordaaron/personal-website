95e4f08c5a9879edea2e5e9cfe0fe6667f7755c7
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-03-09 18:47:16 -0800

    Responsive for larger screens

--- components/service.tsx
+++ components/service.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const Service = (props: { src: string, text: string, className: string, alt: string }) => {
   return (
-    <div className={`flex justify-center items-center gap-5 mb-16 px-3 ${props.className}`}>
-      <img src={props.src} height={124} width={124} alt={props.alt} className="drop-shadow-xl" />
+    <div className={`flex justify-center items-center gap-5 mb-16 px-3 ${props.className} md:text-3xl`}>
+      <img src={props.src} alt={props.alt} className="drop-shadow-xl w-[124px] h-[124px] md:w-[225px] md:h-[225px]" />
       <p className="w-1/2 text-center">{props.text}</p>
     </div>
   )
