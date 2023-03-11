6d878d504e7e6ecccad481e4f659843501904686
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-03-11 09:27:47 -0800

    Margin fix

--- components/service.tsx
+++ components/service.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Service = (props: { src: string, text: string, className: string, alt: string }) => {
   return (
-    <div className={`flex justify-center items-center gap-5 mb-16 px-3 ${props.className} md:text-3xl`}>
+    <div className={`flex justify-center items-center gap-5 mb-8 px-3 ${props.className} md:text-3xl`}>
       <img src={props.src} alt={props.alt} className="drop-shadow-xl w-[124px] h-[124px] md:w-[200px] md:h-[200px]" />
       <p className="w-1/2 text-center">{props.text}</p>
     </div>
