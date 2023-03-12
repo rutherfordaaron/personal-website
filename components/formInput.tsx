d57928f4f6cdad6f9ee1021936f08c6a8e7d282b
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-03-12 06:01:49 -0700

    Contact form
    Button doesn't do anything yet to submit the form and needs better validation

---
+++ components/formInput.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+const FormInput = (props: { label: string, type: string, state: string, setState: Function }) => {
+  const input = "bg-transparent transition-all border-b-2 focus:outline-none focus:border-indigo-300 p-1 pb-0 min-w-[250px]";
+  const activeInput = "border-indigo-800";
+  const inactiveInput = "border-indigo-400";
+
+  const inputContainer = "relative";
+
+  const inactiveLabel = "bottom-[2px] left-[5px]";
+  const activeLabel = "text-xs bottom-[28px] left-0 text-indigo-300";
+  const label = "transition-all absolute"
+
+  const textArea = "bg-transparent border-[2mm] border-slate-800 rounded-2xl p-2 focus:outline-none focus:border-indigo-300 transition-all min-w-[250px] min-h-[250px]"
+
+  const inactiveTextAreaLabel = "top-[23px] left-[20px]"
+  const activeTextAreaLabel = "text-xs top-[-15px] left-[12px] text-indigo-300"
+
+  if (props.type !== "textarea") {
+    return (
+      <div className={inputContainer}>
+        <label htmlFor={props.label} className={`${label} ${props.state ? activeLabel : inactiveLabel}`}>{props.label}</label>
+        <input type={props.type} id={props.label} className={`${input} ${props.state ? activeInput : inactiveInput}`} onChange={e => props.setState(e.target.value)} />
+      </div>
+    )
+  } else {
+    return (
+      <div className={`pt-2 ${inputContainer}`}>
+        <label htmlFor="textarea" className={`${label} ${props.state ? activeTextAreaLabel : inactiveTextAreaLabel}`}>{props.label}</label>
+        <textarea id={props.label} className={`${textArea} ${props.state ? activeInput : inactiveInput}`} onChange={e => props.setState(e.target.value)}></textarea>
+      </div>
+    )
+  }
+}
+
+export default FormInput;
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,11 +1,33 @@
 import Main from "../components/main";
+import FormInput from "../components/formInput";
+import { useState, useEffect } from "react";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [valid, setValid] = useState(false);
+
+  useEffect(() => {
+    if (name && email && message) {
+      setValid(true);
+    } else {
+      setValid(false);
+    }
+  })
+
+  const buttonStyle = "block border-[2mm] px-10 py-5 rounded-full text-2xl tracking-wider md:text-3xl md:py-7 md:px-12 transition-all "
+  const inactiveButtonStyle = "bg-indigo-500 text-indigo-300 border-indigo-800 hover:cursor-not-allowed"
+  const activeButtonStyle = "border-slate-800 bg-violet-200 text-dark hover:bg-violet-100 hover:text-black hover:scale-105 shadow-lg"
+
   return (
     <Main>
-      <div className="min-h-[110vh]">
-        <form>
-          <h1>Hello World!</h1>
+      <div className="min-h-[110vh] flex items-center justify-center">
+        <form id="contact-me-form" className="max-w-[800px] border-[2mm] bg-indigo-600 border-slate-800 rounded-3xl p-8 text-light flex flex-col items-center justify-center gap-8">
+          <FormInput type="text" label="Name*" state={name} setState={setName} />
+          <FormInput type="email" label="Email*" state={email} setState={setEmail} />
+          <FormInput type="textarea" label="Message*" state={message} setState={setMessage} />
+          <button className={`${buttonStyle} ${valid ? activeButtonStyle : inactiveButtonStyle}`}>Submit</button>
         </form>
       </div>
     </Main>
