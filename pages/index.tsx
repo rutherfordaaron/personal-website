aa5ceaa01456fae612dc07639346596c03ef9dc9
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-04-17 06:26:01 -0700

    Landing section
    Created the intro section that includes my headshot, name card and title, and intro. Fully responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,19 +5,25 @@ const Home = () => {
   return (
     <article>
       {/* ---------- Landing Section ---------- */}
-      <section>
-        {/* <div>
-          <p>I'm Aaron Rutherford, a full stack web developer with a focus on Next.js, React, MongoDB, and TailwindCSS. As a professional with a deep passion for web development, I strive to deliver high-quality and responsive web applications. I'm committed to staying up-to-date with the latest web development technologies and trends, and I'm confident in my ability to tackle complex projects and deliver outstanding results. My portfolio showcases my range of capabilities, and I'm ready to take on any challenge and contribute to your team's success.</p>
+      <section className="lg:grid lg:px-44 grid-cols-2 justify-center items-center gap-12">
+        {/* Name Card & Image Wrapper */}
+        <div className="relative flex flex-col justify-center items-center gap-0 md:mt-10">
+          {/* Headshot Image */}
+          <div className="w-56 h-72 md:w-72 md:h-96 shadow-md bg-gray-600 relative right-8 md:right-24">
+            {/* <Image src="/headshot.jpg" alt="Aaron Rutherford Headshot" layout="fill" /> */}
+          </div>
+          {/* Name Card */}
+          <div className="relative bg-gray-200 px-8 py-6 md:px-12 md:py-8 bottom-36 md:bottom-48 -right-8 md:-right-10 shadow-lg z-40">
+            <h1 className="text-xl md:text-2xl">Aaron Rutherford</h1>
+            <p className="text-md md:text-xl text-gray-600 italic">Fullstack Developer</p>
+          </div>
         </div>
-        <div className="relative h-[400px] w-[300px] mx-auto my-4">
-          <Image src="/headshot.jpg" layout="fill" alt="aaron rutherford headshot" />
-        </div> */}
+        {/* Intro */}
+        <p className="lg:w-96 lg:pb-24 indent-10 relative bottom-10 lg:bottom-0">I am passionate about creating sites that are inspiring, intuitive, and innovative. I'm always adding new technologies and techniques to my repretoire to ensure my projects are the best that they can be. I also have a strong belief that everything should be accessible: from they way I write my code in a readable way for others, to the end result that the end-user experiences.</p>
       </section>
 
-      {/* ---------- Tool Carousel Section ---------- */}
-      <section>
-        <Carousel />
-      </section>
+      {/* ---------- Tools Carousel ---------- */}
+      <Carousel />
 
       {/* ---------- Projects Section ---------- */}
       <section id="portfolio">
