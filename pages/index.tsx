a5e127e949f9014c3dd18ace0a5c6ebaf52982b3
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-04-17 18:47:35 -0700

    Copy for all projects and updated links
    Changed my username on github, so I needed to update all the github links. Replaces countries project with the e-commerce project. All other projects now have at least a little bit of copy

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,10 +38,10 @@ const Home = () => {
           src="/screenshots/recipe-box.png"
           alt="recipe box project"
           href="https://recipe-box-phi.vercel.app/"
-          githubHref="https://github.com/hazipan/recipe-box"
+          githubHref="https://github.com/rutherfordaaron/recipe-box"
           title="Recipe Box"
           subtitle="Digitize your recipe book with Recipe Box."
-          body={<p>Recipe Box is a full stack application created using Next.js and TailwindCSS with a custom built authentication API and the ability to store user data by leveraging MongoDB. A few of its features include: a rating system for public recipes, an internal search engine for finding the right recipes, options for making recipes public or private, and easy to navigate froms. A labor of love, Recipe Box is continuously being updated and improved upon. More features are on their way, so check back often.</p>}
+          body={<p>Recipe Box is a full stack application created using Next.js and TailwindCSS with a custom built authentication API and the ability to store user data by leveraging MongoDB. A few of its features include: a rating system for public recipes, an internal search engine for finding the right recipes, options for making recipes public or private, and easy to navigate froms. Being a labor of love, Recipe Box is continuously being updated and improved upon.</p>}
         />
 
         {/* la buca */}
@@ -49,7 +49,7 @@ const Home = () => {
           src="/screenshots/la-buca.png"
           alt="La Buca website"
           href="https://la-buca-italian-cafe.vercel.app/"
-          githubHref="https://github.com/Hazipan/la-buca-italian-cafe"
+          githubHref="https://github.com/rutherfordaaron/la-buca-italian-cafe"
           title="La Buca Italian Restaraunt"
           subtitle="A modernization and reimagination of an existing site."
           body={<p>An innovative redesign of a local restaruant's website. La Buca's website includes menus, review, links to social sites, and directions using Google Maps. Created with Next.js and TailwindCSS, this site in an excellent example of high quality user experience. See the <Link href="https://labucaitaliancafe.com/">original website</Link> for comparison.</p>}
@@ -58,45 +58,45 @@ const Home = () => {
 
         {/* countries api */}
         <ProjectCard
-          src="/screenshots/countries-api.png"
-          alt="countries api project"
-          href="https://hazipan.github.io/countries-project-using-api/"
-          githubHref="https://github.com/Hazipan/countries-project-using-api/"
-          title="Countries of the World"
-          subtitle="Learn interesting things about any country you can think of."
-          body={<p>My first exposure to API's and fetching data from an external source, this project allows you to explore all the countries of the world and learn about things like their currency, population, and what their flag looks like. This project was built with React and React-Router. It's data comes from the <Link href="https://restcountries.com/">Countries API</Link></p>}
+          src="/screenshots/e-commerce.png"
+          alt="static e-ommerce page"
+          href="https://rutherfordaaron.github.io/e-commerce-page/"
+          githubHref="https://github.com/rutherfordaaron/e-commerce-page/"
+          title="Static E-Commerce Page"
+          subtitle="A pixel-perfect creation from a given design."
+          body={<p>Built from a <Link href="https://frontendmentor.io">Frontend Mentor</Link> project, this static web page was created with pixel-perfect precision. Built using React, SCSS, and only a few given images - no boilerplate code was used.</p>}
         />
 
         {/* rock, paper, scissors */}
         <ProjectCard
           src="/screenshots/rock-paper-scissors.png"
           alt="rock paper scissors project"
-          href="https://hazipan.github.io/rock-paper-scissors-lizard-spock/"
-          githubHref="https://github.com/Hazipan/rock-paper-scissors-lizard-spock"
+          href="https://rutherfordaaron.github.io/rock-paper-scissors-lizard-spock/"
+          githubHref="https://github.com/rutherfordaaron/rock-paper-scissors-lizard-spock"
           title="Rock, Paper, Scissors, Lizard, Spock"
           subtitle="An expanded version of the classic game."
-          body={<p>Play against the computer as it randomly picks a symbol to put against you. This project was built with React and plain CSS, and it really tested my CSS animation skills and introduced me to the concept of data persistence. See how high (or low) of a score you can get. The highest so far is 23 and the lowest is -11.</p>}
+          body={<p>Play against the computer as it randomly picks a symbol to put against you. This project was built with React and plain CSS, and shows off complex and well orchestrated CSS animations.</p>}
           reverse={true}
         />
 
         <ProjectCard
           src="/screenshots/tip-calc.png"
           alt="tip splitter project"
-          href="https://hazipan.github.io/tip-calc/"
-          githubHref="https://github.com/Hazipan/tip-calc/"
+          href="https://rutherfordaaron.github.io/tip-calc/"
+          githubHref="https://github.com/rutherfordaaron/tip-calc/"
           title="Tip Splitter"
-          subtitle=""
-          body={<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>}
+          subtitle="Split the tip with this calculator."
+          body={<p>Built using React and CSS, use this simple app to split the bill between multiple people and calculate the tip.</p>}
         />
 
         <ProjectCard
           src="/screenshots/todo.png"
           alt="todo project"
-          href="https://hazipan.github.io/todo-app/"
-          githubHref="https://github.com/Hazipan/todo-app/"
+          href="https://rutherfordaaron.github.io/todo-app/"
+          githubHref="https://github.com/rutherfordaaron/todo-app/"
           title="Todo"
-          subtitle=""
-          body={<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>}
+          subtitle="A simple todo app."
+          body={<p>Track and manage tasks using this simple todo app.</p>}
           reverse={true}
         />
       </section>
